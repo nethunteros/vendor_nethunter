97352165f43193e32b74e751d4018290cfa190cb
binkybear <binkybear@nethunter.com>
2016-10-31 21:21:03 -0500

    Initial CM
    Signed-off-by: binkybear <binkybear@nethunter.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+# OnePlus 1
+add_lunch_combo nethunter_bacon-userdebug
+add_lunch_combo nethunter_bacon-user
+
+# OnePlus 2
+add_lunch_combo nethunter_oneplus2-userdebug
+add_lunch_combo nethunter_oneplus2-user
+
+# OnePlus 3
+add_lunch_combo nethunter_oneplus3-userdebug
+add_lunch_combo nethunter_oneplus3-user
